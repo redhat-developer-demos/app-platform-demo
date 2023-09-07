8599bf1b3e72be13181dc85e978ec8d19a8c0a9b
unknown <dschenck@win.redhat.com>
2023-09-07 11:02:44 -0400

    Added hard-coded message to test pipeline

--- queries.js
+++ queries.js
@@ -9,8 +9,8 @@ const pool = new Pool({
 
 
 const getText = (request, response) => {
-//    pool.query('SELECT * FROM texts ORDER BY random() LIMIT 1', (error, results) => {
-        pool.query('SELECT 99 as id, "HARD-CODED" as pictureText FROM texts LIMIT 1', (error, results) => {
+   pool.query('SELECT * FROM texts ORDER BY random() LIMIT 1', (error, results) => {
+//        pool.query('SELECT 99 as id, "HARD-CODED" as pictureText FROM texts LIMIT 1', (error, results) => {
             if (error) {
             throw error
         }
