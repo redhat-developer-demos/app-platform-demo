140f577e6a7b21066e5a8fd7f649b32a280af8ad
unknown <dschenck@win.redhat.com>
2023-09-07 13:45:48 -0400

    hard-coded as test removed

--- queries.js
+++ queries.js
@@ -10,8 +10,8 @@ const pool = new Pool({
 
 const getText = (request, response) => {
     console.log(`Reading from database...`)
-    //pool.query('SELECT * FROM texts ORDER BY random() LIMIT 1', (error, results) => {
-                pool.query('SELECT 99 as id, "HARD-CODED" as pictureText FROM texts LIMIT 1', (error, results) => {
+    pool.query('SELECT * FROM texts ORDER BY random() LIMIT 1', (error, results) => {
+    //            pool.query('SELECT 99 as id, "HARD-CODED" as pictureText FROM texts LIMIT 1', (error, results) => {
         if (error) {
             throw error
         }
